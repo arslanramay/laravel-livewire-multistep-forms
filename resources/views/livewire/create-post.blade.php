3936270464ad46ef6c72f6b581c4a4632211cc71
Arslan Ramay <arslanramay104@gmail.com>
2024-11-25 17:39:26 +0400

    Added Livewire & Bootstrap CSS

---
+++ resources/views/livewire/create-post.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <div class="mb-3">
+        <label for="description" class="form-label">Post Body</label>
+        <textarea class="form-control" name="description" id="" rows="3"></textarea>
+    </div>
+
+</div>
