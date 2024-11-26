e00568f1a0c0833e85a39edfbcc4a42d1808c445
Arslan Ramay <arslanramay104@gmail.com>
2024-11-26 22:11:44 +0400

    Updated Landing page UI Layout

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,19 @@
 <?php
 
-use App\Livewire\SubscriberForm;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SubscriberController;
 
+// Route for the Landing Page
 Route::get('/', function () {
     return view('welcome');
+    // return view('subscribers-list');
+
 });
 
 
 // Routes for User Onboarding form
 Route::get('/subscriber-onboarding-form', function() {
     return view('subscriberform');
-});
+})->name('subscribers.new');
 
 Route::get('/subscribers-list', [SubscriberController::class, 'index'])->name('subscribers.list');
